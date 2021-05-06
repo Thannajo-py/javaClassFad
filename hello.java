b53e004099e1327b27453d133192c97bf7a94cce
Thannajo-py <wolgan_4@hotmail.com>
2021-05-06 16:40:45 +0200

    end of first day

---
+++ hello.java
@@ -0,0 +1,21 @@
+public class hello
+{
+	public static void main(String[] arg)
+	{
+		System.out.println("Hello ADRAR !");
+		int x = 5;
+		float y = 2.5f;
+		double z = 2.5552;
+		double autre = 0;
+		boolean moiMoi = true;
+		char c5emeLettre = 'e';
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(autre);
+		System.out.println(moiMoi);
+		System.out.println(c5emeLettre);
+		
+		
+	}
+}
